3be9aa567a58a6a684476cd7285d2b9014c23a6d
Joe Paul <joe.paul@fulfil.io>
2016-06-28 11:33:56 +0530

    Initial Boilerplate

---
+++ tests/test_spree.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_spree
+----------------------------------
+
+Tests for `spree` module.
+"""
+
+import pytest 	# noqa
+
+
+from spree import spree
+
+
+class TestSpree(object):
+
+    @classmethod
+    def setup_class(cls):
+        spree
+        pass
+
+    def test_something(self):
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        pass
